a7310ad1944c4b2c4c2b0cccd36be8cb008eea8c
hydrokrak <denta24>
2021-05-01 12:32:52 +0200

    git init

---
+++ precache-manifest.b408f5f478aab14ccf42d0151b69170a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e926e2c754695c2fdbb93f3fd20070f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25e5ab7700ea229d8e4a",
+    "url": "/static/css/2.71bc2c0d.chunk.css"
+  },
+  {
+    "revision": "6e40f56a6bc26ba27380",
+    "url": "/static/css/main.134128e9.chunk.css"
+  },
+  {
+    "revision": "25e5ab7700ea229d8e4a",
+    "url": "/static/js/2.7b51bbdc.chunk.js"
+  },
+  {
+    "revision": "25f4f2b3ca2813a8f26427cfc7e01085",
+    "url": "/static/js/2.7b51bbdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e40f56a6bc26ba27380",
+    "url": "/static/js/main.29f75c43.chunk.js"
+  },
+  {
+    "revision": "4b3363d0f7a27b771f1d",
+    "url": "/static/js/runtime-main.e0fe8454.js"
+  },
+  {
+    "revision": "4fe6f9caff8b287170d51d3d71d5e5c6",
+    "url": "/static/media/lg.4fe6f9ca.ttf"
+  },
+  {
+    "revision": "5fd4c338c1a1b1eeeb2c7b0a0967773d",
+    "url": "/static/media/lg.5fd4c338.woff"
+  },
+  {
+    "revision": "c066c5448562b3ccaefb6408ce4b4ae1",
+    "url": "/static/media/lg.c066c544.svg"
+  },
+  {
+    "revision": "ecff11700aad0000cf3503f537d1df17",
+    "url": "/static/media/lg.ecff1170.eot"
+  }
+]);
